829e648569025ae799d0ca5dac1c3b737e381eac
shini <matteoezio@me.com>
2021-02-17 18:02:17 +0100

    link inseriti

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'ControllerBlog@home')->name('home');
+Route::get('/about', 'ControllerBlog@about')->name('about');
+Route::get('/blog', 'ControllerBlog@blog')->name('blog');
